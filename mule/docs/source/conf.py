9a6e8b5b8d6bdbb358c21be1afea30876039fd12
stevewardle <enquiries@metoffice.gov.uk>
2017-01-03 15:45:46 +0000

    ticket:2636 : Stuart Whitehouse : Update copyright for 2017 : task : mule : regression : trivial
    git-svn-id: https://metomi/svn/um.xm/mule/trunk@32269 0462af51-e8cd-401e-9b71-8f0b910fc1c1

--- mule/docs/source/conf.py
+++ mule/docs/source/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Mule'
-copyright = u'2016, UM Systems Team'
+copyright = u'2017, UM Systems Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
