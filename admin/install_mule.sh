6af77df7e3c512901e29ec7df73515d8044100d1
James Bruten <james.bruten@metoffice.gov.uk>
2025-10-01 15:54:10 +0100

    test

--- admin/install_mule.sh
+++ admin/install_mule.sh
@@ -364,6 +364,7 @@ function unpack_and_copy(){
     if [ ! -d $egg ] ; then
       echo "[INFO] Unpacking zipped egg..."
       find $SCRATCHDIR -name '*.egg*'
+      ls $SCRATCHDIR
       unzip_dir=$SCRATCHLIB/${module}_unzipped_egg
       unzip $egg -d $unzip_dir
       egg=$unzip_dir
