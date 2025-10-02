4c2e0a9122888de4aa7d2c344dd3ca4c7549a92c
Yaswant Pradhan <2984440+yaswant@users.noreply.github.com>
2025-10-02 11:51:25 +0100

    Initial docgen action (#6)

--- mule/lib/mule/tests/unit/test_AncilFile.py
+++ mule/lib/mule/tests/unit/test_AncilFile.py
@@ -319,7 +319,7 @@ class Test_validate(tests.MuleTest):
         self.anc.fields = [self.fld]
         with six.assertRaisesRegex(
             self, ValidateError,
-                "Field start longitude \(bzx\) not RMDI"):
+                r"Field start longitude \(bzx\) not RMDI"):
             self.anc.validate()
 
     # Test a variable resolution field with non RMDI bzy fails
@@ -335,7 +335,7 @@ class Test_validate(tests.MuleTest):
         self.anc.fields = [self.fld]
         with six.assertRaisesRegex(
             self, ValidateError,
-                "Field start latitude \(bzy\) not RMDI"):
+                r"Field start latitude \(bzy\) not RMDI"):
             self.anc.validate()
 
     # Test a variable resolution field with non RMDI bdx fails
@@ -351,7 +351,7 @@ class Test_validate(tests.MuleTest):
         self.anc.fields = [self.fld]
         with six.assertRaisesRegex(
             self, ValidateError,
-                "Field longitude interval \(bdx\) not RMDI"):
+                r"Field longitude interval \(bdx\) not RMDI"):
             self.anc.validate()
 
     # Test a variable resolution field with non RMDI bdy fails
@@ -367,7 +367,7 @@ class Test_validate(tests.MuleTest):
         self.anc.fields = [self.fld]
         with six.assertRaisesRegex(
             self, ValidateError,
-                "Field latitude interval \(bdy\) not RMDI"):
+                r"Field latitude interval \(bdy\) not RMDI"):
             self.anc.validate()
 
     # Test lower boundary x value just within tolerance passes
